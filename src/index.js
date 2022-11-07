7ae1a19a3f377a4f64e8291988c9e9869d181397
Rojan <rojan108p@gmail.com>
2022-11-08 00:07:30 +1100

    Used Props used props in react component

--- src/index.js
+++ src/index.js
@@ -1,33 +1,83 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <div>
-    <h1>My Contacts</h1>
+const ourData = [
+  {
+    name: "Beyonce",
+    src:
+      "https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg",
+    phone: "+123 456 789",
+    e_mail: "b@beyonce.com"
+  },
+  {
+    name: "Jack Bauer",
+    src:
+      "https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg",
+    phone: "+987 654 321",
+    e_mail: "jack@nowhere.com"
+  },
+  {
+    name: "Chuck Norris",
+    src:
+      "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png",
+    phone: "+918 372 574",
+    e_mail: "gmail@chucknorris.com"
+  }
+];
 
-    <h2>Beyonce</h2>
-    <img
-      src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-      alt="avatar_img"
-    />
-    <p>+123 456 789</p>
-    <p>b@beyonce.com</p>
+function OurCard(props) {
+  return (
+    <div>
+      {console.log({ props })}
+      <h2>{props.name}</h2>
+      <img src={props.src} alt="avatar_img" />
+      <p>{props.phone}</p>
+      <p>{props.e_mail}</p>
+    </div>
+  );
+}
 
-    <h2>Jack Bauer</h2>
-    <img
-      src="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
-      alt="avatar_img"
-    />
-    <p>+987 654 321</p>
-    <p>jack@nowhere.com</p>
+function App() {
+  let name, src, phone, e_mail;
+  var count_ = 0;
 
-    <h2>Chuck Norris</h2>
-    <img
-      src="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
-      alt="avatar_img"
-    />
-    <p>+918 372 574</p>
-    <p>gmail@chucknorris.com</p>
-  </div>,
-  document.getElementById("root")
-);
+  return (
+    <div>
+      <h1>My Contacts</h1>
+      <OurCard
+        key={count_}
+        name={ourData[0].name}
+        src={ourData[0].src}
+        phone={ourData[0].phone}
+        e_mail={ourData[0].e_mail}
+      />
+      {console.log(ourData[0])}
+      <br />
+      <span
+        style={{ border: "3px solid black", padding: "0.5rem", color: "green" }}
+      >
+        Below using array.map{" "}
+      </span>
+      {/* why can't we use array.forEach ?? */}
+      {ourData.map((x) => {
+        name = x.name;
+        src = x.src;
+        phone = x.phone;
+        e_mail = x.e_mail;
+        count_++;
+
+        return (
+          <OurCard
+            key={count_}
+            name={name}
+            src={src}
+            phone={phone}
+            e_mail={e_mail}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
